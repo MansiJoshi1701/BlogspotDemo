84defacec8f1fce9331bcd399067d545032339a4
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-01-28 20:21:29 -0800

    Skeletal setup of the app

---
+++ src/BlogList.js
@@ -0,0 +1,20 @@
+const BlogList = ( {Blogs , title , handleDelete} ) => {
+
+    // const blogs = props.blogs;
+    // const title = props.title;
+
+    return (
+        <div className="blog-list">
+            <h2>{ title }</h2>  
+            {Blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+ 
+export default BlogList;
