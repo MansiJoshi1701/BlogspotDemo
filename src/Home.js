4d936d54640950bdf4aac60c13083aefa48a78da
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-01-31 20:55:47 -0800

    added Fetch error handling

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,9 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs , setBlogs] = useState(null)
+    const [blogs , setBlogs] = useState(null);
+    const [isPending , setIsPending] = useState(true);
+    const [error , setError] = useState(null);
     
 
     const handleDelete = (id) => {
@@ -18,15 +20,27 @@ const Home = () => {
         //If I only want it to run once on initial render, then pass and empty dependancy array []
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch data for that resource');
+                }
                 return res.json(); //parses the res object into a json object
             })
             .then(data => { //this will fire once above then function is executed i.e. once res is fully received
                 setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+
             })
     } , [])
 
     return ( 
         <div className="home">
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading. . .</div>}
             {blogs && <BlogList Blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
             
         </div>
