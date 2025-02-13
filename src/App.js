26efa54e54574a949aee51ee33f9b3f067c6b473
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-02-12 21:08:00 -0800

    Reusing custon hooks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router , Route , Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 //We've surrounded our entire app with <Router> so that all components that are nested inside the app
 //component will have access to the router
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path = "/create">
               <Create />
+            </Route>
+            <Route path = "/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ( {Blogs , title} ) => {
 
-    // const blogs = props.blogs;
-    // const title = props.title;
+//Since this is being called in the HOME section so this is a blueprint of how all blogs 
+//will be displayed/rendered in the home page
 
     return (
         <div className="blog-list">
             <h2>{ title }</h2>  
             {Blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by {blog.author}</p>
+                <div className="blog-preview">
+                    <Link to = {`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written by {blog.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
